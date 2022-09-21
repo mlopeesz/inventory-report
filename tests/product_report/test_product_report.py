57b6b74132dc8afd0fb3181dc1ad7c92435d5cd9
Mauricio Lopes <mlopeesz@gmail.com>
2022-09-21 08:57:36 -0300

    test: :white_check_mark: implements test_relatorio_produto

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,23 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    produto = Product(
+        1,
+        "Gelobol",
+        "AloAmbev",
+        "10-05-2022",
+        "10-05-2025",
+        "A1B2C3D4E5",
+        "Com pressão",
+    )
+
+    result = (
+        f"O produto {produto.nome_do_produto}"
+        f" fabricado em {produto.data_de_fabricacao}"
+        f" por {produto.nome_da_empresa} com validade"
+        f" até {produto.data_de_validade}"
+        f" precisa ser armazenado {produto.instrucoes_de_armazenamento}."
+    )
+
+    assert produto.__repr__() == result
